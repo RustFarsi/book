bc29557316694cb7742504a9e2644bb83e1e3faa
Pouria Mokhtari <contact@pouriamokhtari.ir>
2020-04-11 16:44:27 +0430

    Update book

---
+++ FARSI/github_pages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mdbook build
+mv book /tmp
+cd ..
+git checkout gh-pages
+mv /tmp/book .
+git add -A
+git commit -m "Update book"
+git push origin gh-pages
+git checkout master
+cd FARSI
+mdbook clean
